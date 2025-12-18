71dde27470b2b4cf11d23961f16278a15724b331
Guyak <guyotantonin2001@gmail.com>
2025-12-18 13:55:39 +0100

    read from project opcua server

--- set_opcua.py
+++ set_opcua.py
@@ -28,18 +28,24 @@ print(f"Connected !")
 ##————————————————————————————————————————————————————————————————————————————##
 ## Reading/Writing values of the server
 
-nodeBool = client.get_node(f'ns=2;s=Local HMI.Tags.test_bool')
-nodeString = client.get_node(f'ns=2;s=Local HMI.Tags.test_string')
 
-switch = nodeBool.get_value()
-if switch:
-    nodeBool.set_value(ua.DataValue(ua.Variant(False, ua.VariantType.Boolean)))
-    nodeString.set_value(ua.DataValue(ua.Variant(f'Mis à faux - Date : {time.strftime("%Y-%m-%d_%H-%M-%S")}', ua.VariantType.String)))
-else :
-    nodeBool.set_value(ua.DataValue(ua.Variant(True, ua.VariantType.Boolean)))
-    nodeString.set_value(ua.DataValue(ua.Variant(f"Mis à vrai - Date : {time.strftime("%Y-%m-%d_%H-%M-%S")}", ua.VariantType.String)))
+# nodeBool = client.get_node(f'ns=2;s=Local HMI.Tags.test_bool')
+# nodeString = client.get_node(f'ns=2;s=Local HMI.Tags.test_string')
 
-print(nodeString.get_value())
+# switch = nodeBool.get_value()
+# if switch:
+    # nodeBool.set_value(ua.DataValue(ua.Variant(False, ua.VariantType.Boolean)))
+    # nodeString.set_value(ua.DataValue(ua.Variant(f'Mis à faux - Date : {time.strftime("%Y-%m-%d_%H-%M-%S")}', ua.VariantType.String)))
+# else :
+    # nodeBool.set_value(ua.DataValue(ua.Variant(True, ua.VariantType.Boolean)))
+    # nodeString.set_value(ua.DataValue(ua.Variant(f"Mis à vrai - Date : {time.strftime("%Y-%m-%d_%H-%M-%S")}", ua.VariantType.String)))
+# print(nodeString.get_value())
+
+nodeInt16 = client.get_node(f'ns=2;s=API_425056.Tags.Recette.GEN.Limite_Couple')
+print(f'Limite_Couple à {time.strftime("%Y-%m-%d_%H-%M-%S")} : {nodeInt16.get_value()}')
+
+nodeUInt16 = client.get_node(f'ns=2;s=API_425056.Tags.Recette.VIDE.Tension_Accept_1')
+print(f'Tension_Accept_1 à {time.strftime("%Y-%m-%d_%H-%M-%S")} : {nodeUInt16.get_value()/10}')
 
 ##————————————————————————————————————————————————————————————————————————————##
 ## Disconnecting from the server
