4510f4162435724cf263e09e10c8e0bf9a9c8a06
Guyak <guyotantonin2001@gmail.com>
2025-12-19 15:34:39 +0100

    creation header & footer

--- generate_pdf.py
+++ generate_pdf.py
@@ -1,7 +1,43 @@
 from fpdf import FPDF
+import time
+import os
 
-pdf = FPDF()
+class PDF(FPDF):
+    def header(self):
+        # Rendering logo:
+        self.image("./images/SNCF.png", 5,5,20)
+        # Setting font: helvetica bold 15
+        self.set_font("helvetica", style="B", size=15)
+        # Moving cursor to the right:
+        self.cell(80)
+        # Printing title:
+        self.cell(30, 10, "Title", border=1, align="C")
+        self.cell(20, 10, "yes", border=1)
+        # Performing a line break:
+        self.ln(20)
+
+    def footer(self):
+        # Position cursor at 1.5 cm from bottom:
+        self.set_y(-15)
+        # Setting font: helvetica italic 8
+        self.set_font("helvetica", style="I", size=8)
+        # Printing page number:
+        self.cell(0, 10, f"{time.strftime("%Y/%m/%d")}", align="C")
+        self.set_y(-15)
+        self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", align="L")
+
+
+# Instantiation of inherited class
+pdf = PDF()
 pdf.add_page()
-pdf.set_font('Helvetica', size=12)
-pdf.cell(text="Hello world!")
-pdf.output("hello_world.pdf")
+pdf.set_font("helvetica", size=12)
+for i in range(1, 41):
+    pdf.cell(0, 10, f"Printing line number {i}", new_x="LMARGIN", new_y="NEXT")
+
+# Create report path based on specimen type, date and time
+path = f'./Regiolis Mot/{time.strftime("%Y_%m_%d")}/{time.strftime("%H_%M_%S")}'
+# For debug, easy path
+path = './'
+if not os.path.exists(path):
+    os.makedirs(path)
+pdf.output(f"{path}/report.pdf")
