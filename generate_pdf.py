a47c1971391486eee08b8f2e6153dea1e6b852b4
Guyak <guyotantonin2001@gmail.com>
2025-12-22 10:41:35 +0100

    ajout colonne Go/NoGo + nettoyage code
    fctn essai à vide COMPLETE

--- generate_pdf.py
+++ generate_pdf.py
@@ -2,44 +2,125 @@ from fpdf import FPDF
 import time
 import os
 
-# Définition des fonctions de création de rapport pour chaque essai
-def print_Vide(pdf, type_specimen, vitesse_1, vitesse_2, vitesse_3, tension_accep_1, tension_accep_2, tension_accep_3, hyst_1, hyst_2, hyst_3, tension_1, tension_2, tension_3):
-    tension_min_1 = round((tension_accep_1-hyst_1)/10, 1)
-    tension_max_1 = round((tension_accep_1+hyst_1)/10, 1)
-    tension_min_2 = round((tension_accep_2-hyst_2)/10, 1)
-    tension_max_2 = round((tension_accep_2+hyst_2)/10, 1)
-    tension_min_3 = round((tension_accep_3-hyst_3)/10, 1)
-    tension_max_3 = round((tension_accep_3+hyst_3)/10, 1)
+##————————————————————————————————————————————————————————————————————————————##
+## Creation du document PDF
+class PDF(FPDF):
+    def __init__(self, titre, serie, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.titre = titre
+        self.serie = serie
 
-    pdf.set_font("helvetica", style="B", size=12)
-    pdf.cell(0, 10, f"2 - Essai à vide", new_y="NEXT")
+    def header(self):
+        # Logo
+        self.image("./_images/SNCF.png", 5,5,20)
+        # Police titre
+        self.set_font("helvetica", style="B", size=12)
+        # Bouger le curseur à droite
+        self.cell(50)
+        # Titre
+        self.cell(80, 5, self.titre, align="C", new_x="LEFT", new_y="NEXT")
+        self.cell(80, 5, self.serie, align="C")
+        # Saut de ligne
+        self.ln(20)
+
+    def footer(self):
+        # Bouger le curseur à 1.5cm du bas de page
+        self.set_y(-15)
+        # Police footer
+        self.set_font("helvetica", style="I", size=8)
+        # Numéro de page
+        self.cell(0, 10, f"{time.strftime("%Y/%m/%d")}", border=1, align="C")
+        self.set_y(-15)
+        self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", align="L")
+        self.set_y(-15)
+        self.cell(0, 10, f"{self.serie}", align="R")
+
+def init_pdf(type_specimen, ref_specimen, serie_specimen):
+    pdf = PDF(titre=f"{type_specimen} - {ref_specimen}", serie=f"Moteur N°{serie_specimen}")
+    pdf.add_font('DejaVu', '', './_fonts/DejaVuSans.ttf', uni=True)
+    pdf.add_page()
     pdf.set_font("helvetica", size=12)
-    with pdf.table(text_align="CENTER", first_row_as_headings=False) as table:
-        row = table.row()
-        row.cell("Vitesse entrainement (tr/min)", rowspan=2)
-        row.cell("Tension_moy. (V)", colspan=3)
+    return pdf
 
-        row = table.row()
-        row.cell("Min")
-        row.cell("Mesure")
-        row.cell("Max")
+##————————————————————————————————————————————————————————————————————————————##
+## Valeurs utiles
+def util_pdf(pdf):
+    # Largeur totale disponible pour la rédaction des essais
+    page_largeur = pdf.w - 20
+    # Largeur des colonnes réservée à la détermination des essais
+    go_nogo_largeur = 10
+    tableau_largeur = page_largeur - (2 * go_nogo_largeur)
+    go_nogo_x = tableau_largeur + 10
+    return [page_largeur, go_nogo_largeur, tableau_largeur, go_nogo_x]
 
-        row = table.row()
-        row.cell(str(vitesse_1))
-        row.cell(str(tension_min_1))
-        row.cell(str(tension_1))
-        row.cell(str(tension_max_1))
-        if type_specimen == "Regio2N":
-            row = table.row()
-            row.cell(str(vitesse_2))
-            row.cell(str(tension_min_2))
-            row.cell(str(tension_2))
-            row.cell(str(tension_max_2))
+##————————————————————————————————————————————————————————————————————————————##
+## Définition des fonctions de création de rapport pour chaque essai
+def print_Vide(pdf, type_specimen, vitesse, tension_accep, hyst, tension, go_nogo):
+    ## Récupération de valeurs utilitaires
+    [page_largeur, go_nogo_largeur, tableau_largeur, go_nogo_x] = util_pdf(pdf)
 
-            row = table.row()
-            row.cell(str(vitesse_3))
-            row.cell(str(tension_min_3))
-            row.cell(str(tension_3))
-            row.cell(str(tension_max_3))
-    pdf.ln(10)
+    ## Titre
+    pdf.set_font("helvetica", style="B", size=12)
+    pdf.cell(0, 10, f"2 - Essai à vide", new_x="LMARGIN", new_y="NEXT")
+    pdf.set_font("helvetica", size=12)
+
+    ## Calcul de largeur des colonnes
+    col_widths = [tableau_largeur / 4] * 4
+
+    ## Création de l'en-tête
+    # Sauvegarde des valeurs xy pour réalignement après utilisation de multi-cell
+    [x, y] = [pdf.get_x(), pdf.get_y()]
+    pdf.multi_cell(col_widths[0], 10, "Vitesse entrainement (tr/min)", border=1, align='C')
+    pdf.set_xy(x + col_widths[0], y)
+    pdf.cell(col_widths[1] + col_widths[2] + col_widths[3], 10, "Tension moy. (V)", border=1, align='C')
+    pdf.ln()
+    pdf.cell(col_widths[0], 10, "", border=0)  # cellule vide invisible sous "Vitesse"
+    for val in ["Min", "Mesure", "Max"]:
+        pdf.cell(col_widths[1], 10, val, border=1, align='C')
+    pdf.ln()
+
+    ## Préparation des données 
+    # Calcul des valeurs min et max à afficher
+    tension_min = []
+    tension_max = []
+    for idx in range(3):
+        tension_min.append(round((tension_accep[idx]-hyst[idx])/10, 1))
+        tension_max.append(round((tension_accep[idx]+hyst[idx])/10, 1))
+    
+    # Préparation des données pour chaque vitesse d'essai
+    if type_specimen == "Regio2N":
+        n_essai = 3
+    else:
+        n_essai = 1
+    data = []
+    for idx in range(n_essai):
+        essai = []
+        essai.append(str(vitesse[idx]))
+        essai.append(str(tension_min[idx]))
+        essai.append(str(tension[idx]))
+        essai.append(str(tension_max[idx]))
+        if go_nogo[idx]:
+            essai.append("☑")
+            essai.append("☐")
+        else:
+            essai.append("☐")
+            essai.append("☑")
+        data.append(essai)
+
+    # Création du tableau
+    for row in data:
+        # Colonnes principales
+        pdf.cell(col_widths[0], 10, row[0], border=1, align='C')  # Vitesse
+        pdf.cell(col_widths[1], 10, row[1], border=1, align='C')  # Min
+        pdf.cell(col_widths[2], 10, row[2], border=1, align='C')  # Mesure
+        pdf.cell(col_widths[3], 10, row[3], border=1, align='C')  # Max
+
+        # Colonne Go/NoGo sans bordure, position fixe
+        pdf.set_x(go_nogo_x)
+        pdf.set_font('DejaVu', '', 15)
+        pdf.cell(go_nogo_largeur, 10, row[4], border=0, align='C')
+        pdf.cell(go_nogo_largeur, 10, row[5], border=0, align='C')
+        pdf.set_font("helvetica", size=12)
+        pdf.ln()
+
     return pdf
--- main.py
+++ main.py
@@ -44,36 +44,6 @@ rapport_liste = listes_param["rapport_liste"]
 rapport = Dictionnaire(dict.fromkeys(rapport_liste, -1))
 
 ##————————————————————————————————————————————————————————————————————————————##
-## Creation du document PDF
-class PDF(FPDF):
-    def header(self):
-        # Rendering logo:
-        self.image("./images/SNCF.png", 5,5,20)
-        # Setting font: helvetica bold 15
-        self.set_font("helvetica", style="B", size=15)
-        # Moving cursor to the right:
-        self.cell(80)
-        # Printing title:
-        self.cell(30, 10, "Title", border=1, align="C")
-        self.cell(20, 10, "yes", border=1)
-        # Performing a line break:
-        self.ln(20)
-
-    def footer(self):
-        # Position cursor at 1.5 cm from bottom:
-        self.set_y(-15)
-        # Setting font: helvetica italic 8
-        self.set_font("helvetica", style="I", size=8)
-        # Printing page number:
-        self.cell(0, 10, f"{time.strftime("%Y/%m/%d")}", align="C")
-        self.set_y(-15)
-        self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", align="L")
-
-pdf = PDF()
-pdf.add_page()
-pdf.set_font("helvetica", size=12)
-
-##————————————————————————————————————————————————————————————————————————————##
 ## Mise en place du client OPCUA
 url = credentials["serveur_url"]
 client = Client(url)
@@ -86,14 +56,13 @@ except ConnectionRefusedError:
     sys.exit(1)
 
 printc(f"[green]Connecté !\n")
-print(f"Appuyer sur CTRL-C pour arrêter le programme\n")
 
 ##————————————————————————————————————————————————————————————————————————————##
-## Lecture/Ecrituredes valeurs du serveur
+## Lecture/Ecriture des valeurs du serveur
 API_Lecture = client.get_node(f'ns=2;s=API_425056.Tags.Commande_PC.Lecture')
-
 read_full = False
 
+print(f"Appuyer sur CTRL-C pour arrêter le programme\n")
 try:
     while True:
         if API_Lecture.get_value():
@@ -113,6 +82,10 @@ try:
                 print(f"{idx}/{len(recette_filtre)}", end="\r")
                 setattr(recette, i, client.get_node(f'ns=2;s=API_425056.Tags.Recette.{i}').get_value())
             printc(f'[green]OK   ')
+
+            # Initialisation du fichier PDF
+            pdf = init_pdf("Regio2N", "MJP 250-2", "123456-789")
+
             # Rapport de test
             printc(f'[bright_cyan]Récupération des valeurs de rapport...')
             for idx,i in enumerate(rapport_filtre, start=1):
@@ -120,28 +93,31 @@ try:
                 setattr(rapport, i, client.get_node(f'ns=2;s=API_425056.Tags.Rapport.{i}').get_value())
             printc(f'[green]OK   \n')
 
-            # Affichage de valeurs
-            pdf = print_Vide(pdf,"Regio2N", recette.VIDE_Vitesse_Entrainement_1, recette.VIDE_Vitesse_Entrainement_2, recette.VIDE_Vitesse_Entrainement_3,
-                                            recette.VIDE_Tension_Accept_1, recette.VIDE_Tension_Accept_2, recette.VIDE_Tension_Accept_3,
-                                            rapport.VIDE_Hyst_1, rapport.VIDE_Hyst_2, rapport.VIDE_Hyst_3,
-                                            rapport.VIDE_Tension_1, rapport.VIDE_Tension_2, rapport.VIDE_Tension_3)
-            print(f"Essai à vide rédigé")
+            # Affichage de valeurs - Essai à vide
+            pdf = print_Vide(pdf,"Regio2N", [recette.VIDE_Vitesse_Entrainement_1, recette.VIDE_Vitesse_Entrainement_2, recette.VIDE_Vitesse_Entrainement_3],
+                                            [recette.VIDE_Tension_Accept_1, recette.VIDE_Tension_Accept_2, recette.VIDE_Tension_Accept_3],
+                                            [rapport.VIDE_Hyst_1, rapport.VIDE_Hyst_2, rapport.VIDE_Hyst_3],
+                                            [rapport.VIDE_Tension_1, rapport.VIDE_Tension_2, rapport.VIDE_Tension_3],
+                                            [True, False, True])
+            printc(f"[green]Essai à vide rédigé")
 
             # Remise à 0 du bit de lecture
             API_Lecture.set_value(ua.DataValue(ua.Variant(False, ua.VariantType.Boolean)))
+
+            # Création de dossier basé sur le type de spécimen, la date et l'heure de l'essai
+            path = f'./XXXX/{time.strftime("%Y_%m_%d")}/{time.strftime("%H_%M_%S")}'
+            # Debug
+            path = './Regio2N'
+            if not os.path.exists(path):
+                os.makedirs(path)
+            # Génération du fichier PDF
+            printc(f"[bright_cyan]Création du PDF au chemin {path}...")
+            pdf.output(f"{path}/report.pdf")
+            printc(f'[green]OK\n')
+            break
 except KeyboardInterrupt:
     printc(f"[bright_cyan]Arrêt du programme par l'utilisateur")
     pass
-
-# Create report path based on specimen type, date and time
-path = f'./Regiolis Mot/{time.strftime("%Y_%m_%d")}/{time.strftime("%H_%M_%S")}'
-# For debug, easy path
-path = './Regiolis'
-if not os.path.exists(path):
-    os.makedirs(path)
-printc(f"[bright_cyan]Création du PDF au chemin {path}...")
-pdf.output(f"{path}/report.pdf")
-printc(f'[green]OK\n')
 
 ##————————————————————————————————————————————————————————————————————————————##
 ## Déconnexion du serveur
